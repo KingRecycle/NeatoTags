301e61f8ff18d905ca4a935fe0b04fcc873cbbf3
Charlie <chaoticcow@gmail.com>
2022-10-07 21:30:09 -0400

    Tracker script for Tagger components.

---
+++ Assets/CharlieMadeAThing/NeatoTags/Core/Scripts/NeatoTagTaggerTracker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace CharlieMadeAThing.NeatoTags.Core {
+    public static class NeatoTagTaggerTracker {
+        static readonly HashSet<Tagger> TAGGERS = new();
+        
+        public static void RegisterTagger( Tagger tagger ) {
+            TAGGERS.Add( tagger );
+        }
+
+        public static void UnregisterTagger( Tagger tagger ) {
+            TAGGERS.Remove( tagger );
+        }
+
+        public static void RegisterTaggersInScene() {
+            var taggers = GetAllObjectsOnlyInScene();
+            foreach ( var tagger in taggers ) {
+                RegisterTagger( tagger );
+            }
+        }
+
+        public static void SelectAllGameObjectsWithTaggerThatHasTag( NeatoTag tag ) {
+            Selection.objects = TAGGERS
+                .Where( tagger => tagger.HasTag( tag ) )
+                .Select( tagger => tagger.gameObject )
+                .ToArray();
+        }
+
+        static List<Tagger> GetAllObjectsOnlyInScene()
+        {
+            var objectsInScene = new List<Tagger>();
+
+            foreach (var tagger in (Tagger[]) Resources.FindObjectsOfTypeAll(typeof(Tagger)))
+            {
+                if (!EditorUtility.IsPersistent(tagger.transform.root.gameObject) && tagger.hideFlags is not (HideFlags.NotEditable or HideFlags.HideAndDontSave))
+                    objectsInScene.Add(tagger);
+            }
+
+            return objectsInScene;
+        }
+    }
+}
